7b00c234ac0a2c00312e69e5bfcaaea1bd6f9bd0
alssanchezvega12-jpg <alssanchezvega12@gmail.com>
2026-06-22 16:37:45 -0600

    Actualización estilos morados CRUD

--- server.js
+++ server.js
@@ -10,8 +10,6 @@ app.use(express.static('public'));
 
 // Conexión a MongoDB Atlas
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
 })
 .then(() => console.log("✅ Conectado a MongoDB Atlas"))
 .catch(err => console.error("❌ Error de conexión:", err));
