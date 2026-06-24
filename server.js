b44e4bc003c50891002c26482cfbfdd9d3c14e25
alssanchezvega12-jpg <alssanchezvega12@gmail.com>
2026-06-24 13:10:58 -0600

    Actualización

--- server.js
+++ server.js
@@ -12,13 +12,12 @@ app.use(express.static('public'));
 // Servir carpeta uploads como pública
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
+console.log("🔍 URI detectada:", process.env.MONGO_URI);
+
 // Conexión a MongoDB Atlas
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log("✅ Conectado a MongoDB Atlas"))
-.catch(err => console.error("❌ Error de conexión:", err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ Conectado a MongoDB Atlas"))
+  .catch(err => console.error("❌ Error de conexión:", err));
 
 // Esquema y modelo
 const multimediaSchema = new mongoose.Schema({
